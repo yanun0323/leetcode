347ec229dd55a6c7fc5f9d4af7d933953c25e929
yanun0323 <a1256212562@gmail.com>
2022-08-27 22:07:49 +0800

    pass 0076

--- 0076_Minimum_Window_Substring/0076.go
+++ 0076_Minimum_Window_Substring/0076.go
@@ -2,10 +2,7 @@ package main
 
 //"ADOBECODEBANC", t = "ABC"
 func minWindow(s string, t string) string {
-	if len(s) < len(t) {
-		return ""
-	}
-	if len(s) == 0 || len(t) == 0 {
+	if len(s) < len(t) || len(s) == 0 || len(t) == 0 {
 		return ""
 	}
 
@@ -15,49 +12,38 @@ func minWindow(s string, t string) string {
 	}
 
 	matched := 0
-	shorter := [2]int{0, len(s)}
+	shorter := [2]int{0, len(s) - 1}
 	notFound := true
 	pMap := make(map[byte]int, 52)
 	for lp, rp := 0, 0; rp < len(s); rp++ {
-
 		if tMap[s[rp]] > 0 {
 			pMap[s[rp]]++
 			if pMap[s[rp]] <= tMap[s[rp]] {
 				matched++
 			}
-			if pMap[s[rp]] > tMap[s[rp]] && s[rp] == s[lp] {
-				matched--
+		}
+
+		for ; matched == len(t) && (NextFulfill(&lp, pMap, tMap, s)); lp++ {
+			if tMap[s[lp]] > 0 {
 				pMap[s[lp]]--
-				lp++
-			}
-		}
-
-		if matched == 0 {
-			lp++
-			continue
-		}
-
-		if matched == len(t) {
-			if rp-lp <= shorter[1]-shorter[0]-1 {
-				notFound = false
-				shorter[0] = lp
-				shorter[1] = rp + 1
-			}
-
-			for lp < rp && (matched == len(t) || tMap[s[lp]] == 0) {
-				if tMap[s[lp]] > 0 {
-					if pMap[s[lp]] <= tMap[s[lp]] {
-						matched--
-					}
-					pMap[s[lp]]--
+				if pMap[s[lp]] < tMap[s[lp]] {
+					matched--
 				}
-				lp++
 			}
 		}
-
+		if matched == len(t) && rp-lp <= shorter[1]-shorter[0] {
+			notFound = false
+			shorter[0] = lp
+			shorter[1] = rp
+		}
 	}
 	if notFound {
 		return ""
 	}
-	return s[shorter[0]:shorter[1]]
+	return s[shorter[0] : shorter[1]+1]
+}
+
+// 拿掉現在這個，也會符合t的話
+func NextFulfill(lp *int, pMap, tMap map[byte]int, s string) bool {
+	return tMap[s[*lp]] == 0 || pMap[s[*lp]] > tMap[s[*lp]]
 }
