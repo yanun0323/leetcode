ee6e599084234d1ecba708527afc4d5f684640ec
yanun <yanunyang@psp-power.com.tw>
2022-10-04 18:16:28 +0800

    update today

---
+++ 0112_Path_Sum/0112.go
@@ -0,0 +1,19 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return targetSum-root.Val == 0
+	}
+
+	return hasPathSum(root.Right, targetSum-root.Val) || hasPathSum(root.Left, targetSum-root.Val)
+}
